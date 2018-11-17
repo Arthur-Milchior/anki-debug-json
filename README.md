22be1a0582f3e12b797b54ed685df6c10f52d164
Arthur-Milchior <arthur@milchior.fr>
2018-11-17 06:15:03 +0100

    Printing past value of the add-on

---
+++ README.md
@@ -0,0 +1,25 @@
+# Show JSON error message when importing
+## Rationale
+For some reason, anki use json for config. If json is not parseable,
+(which is often, since it may be hand-written), it generates an
+error-message. And Anki choose to hide it, considering instead that
+the config is empty !
+## Usage
+This add-on correct this problem by showing json's error message,
+allowing the user to correct their JSON. It also print the past
+config, so if you add-on destroy it, it is not lost.
+## Internal
+This add-on change AddonManager.addonMeta and
+AddonManager.addonConfigDefaults in module aqt.addons. It does not
+call the last version, and thus is incompatible with any other add-ons
+modifying this.
+
+## Links, licence and credits
+
+Key         |Value
+------------|-------------------------------------------------------------------
+Copyright   | Arthur Milchior <arthur@milchior.fr>
+Based on    | Anki code by Damien Elmes <anki@ichi2.net>
+License     | GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+Source in   | https://github.com/Arthur-Milchior/anki-debug-json
+Addon number| [2061352905](https://ankiweb.net/shared/info/2061352905)
--- __init__.py
+++ __init__.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright: Arthur Milchior <arthur@milchior.fr>
+# Based on anki code by Damien Elmes <anki@ichi2.net>
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+# Source in https://github.com/Arthur-Milchior/anki-debug-json
+# Addon number 2061352905
+
 import json
 import os
 import sys
@@ -13,11 +20,15 @@ name/directory is dir.
     path = os.path.join(self.addonsFolder(dir), "config.json")
     try:
         with open(path, encoding="utf8") as f:
+            t = f.read()
             try:
-                return json.load(f)
+                return json.loads(t)
             except Exception as e:
-                print(f"Here is a JSON error in addon {dir}:", file=sys.stderr)
+                print(
+                    f"Here is a JSON error in default config of addon {dir}:", file=sys.stderr)
                 print(str(e), file=sys.stderr)
+                print(
+                    f"\n\n===================\n\nCopy and save past config to be sure that it is not overwritten by accident. Past config was {t}", file=sys.stderr)
                 return dict()
     except:
         return None
@@ -27,10 +38,15 @@ def addonMeta(self, dir):
     path = self._addonMetaPath(dir)
     try:
         with open(path, encoding="utf8") as f:
+            t = f.read()
             try:
-                return json.load(f)
+                return json.loads(t)
             except Exception as e:
+                print(
+                    f"Here is a JSON error in current config of addon {dir}:", file=sys.stderr)
                 print(str(e), file=sys.stderr)
+                print(
+                    f"\n\n===================\n\nCopy and save past config to be sure that it is not overwritten by accident. Past config was {t}", file=sys.stderr)
                 return dict()
     except:
         return dict()
